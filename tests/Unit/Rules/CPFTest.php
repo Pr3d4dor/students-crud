1d5d90647d10c2f5422be060987bed7a571770b8
Gianluca Bine <gian_bine@hotmail.com>
2019-12-12 23:29:24 -0300

    Implementado teste unitário de regra de validação de CPF

---
+++ tests/Unit/Rules/CPFTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Unit\Rules;
+
+use App\Rules\CPF;
+use PHPUnit\Framework\TestCase;
+
+class CPFTest extends TestCase
+{
+    protected $cpfRule;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->cpfRule = new CPF();
+    }
+
+    /**
+     * Check that valid cpfs passes
+     *
+     * @dataProvider validCPFs
+     * @param int|float $number
+     * @return void
+     */
+    public function testValidCPFsPasses($cpf)
+    {
+        $this->assertTrue($this->cpfRule->passes('test', $cpf));
+    }
+
+    /**
+     * Check that invalid cpfs fails
+     *
+     * @dataProvider invalidCPFs
+     * @param int|float $number
+     * @return void
+     */
+    public function testInvalidCPFsFail($cpf)
+    {
+        $this->assertFalse($this->cpfRule->passes('test', $cpf));
+    }
+
+    public function validCPFs()
+    {
+        yield ['088.463.559-70'];
+        yield ['08846355970'];
+    }
+
+    public function invalidCPFs()
+    {
+        yield [''];
+        yield [' '];
+        yield ['123.456.789-10'];
+        yield ['12345678910'];
+    }
+}
