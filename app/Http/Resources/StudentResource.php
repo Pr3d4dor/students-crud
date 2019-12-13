af5a2d27ab20f4762204de8c7433f417aa776713
Gianluca Bine <gian_bine@hotmail.com>
2019-12-13 13:56:39 -0300

    Implementado resource de estudante (student)

---
+++ app/Http/Resources/StudentResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'birthdate' => $this->birthdate,
+            'grade' => $this->grade,
+            'postcode' => $this->postcode,
+            'street' => $this->street,
+            'number' => $this->number,
+            'complement' => $this->complement,
+            'neighborhood' => $this->neighborhood,
+            'city' => $this->city,
+            'state' => $this->state,
+            'mother_name' => $this->mother_name,
+            'mother_cpf' => $this->mother_cpf,
+            'preferred_day_for_monthly_payment' => $this->preferred_day_for_monthly_payment,
+        ];
+    }
+}
