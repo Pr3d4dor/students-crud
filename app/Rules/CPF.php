ee9a3cf8876f08fad48b95f0bf79530513247c9b
Gianluca Bine <gian_bine@hotmail.com>
2019-12-12 23:28:26 -0300

    Implementado regra de validação de CPF

---
+++ app/Rules/CPF.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class CPF implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $cpf = preg_replace('/[^0-9]/', '', (string) $value);
+
+        if (strlen($cpf) !== 11 || preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf{$c} * (($t + 1) - $c);
+            }
+
+            $d = ((10 * $d) % 11) % 10;
+
+            if ($cpf{$c} != $d) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'O campo :attribute precisa ser um CPF válido.';
+    }
+}
