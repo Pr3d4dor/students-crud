731f9b62acc26a40a487dfb659b5712dccffccf6
Gianluca Bine <gian_bine@hotmail.com>
2019-12-20 16:12:18 -0300

    Implementado seeder de student

---
+++ database/seeds/StudentsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Student;
+use Illuminate\Database\Seeder;
+
+class StudentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Student::class, 10)->create();
+    }
+}
